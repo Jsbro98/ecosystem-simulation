357dfc0ff0ecceabc8d8069584735da2f9d0dd6b
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-11-30 03:18:51 -0600

    added traits header with traits namespace

---
+++ ecosystem-simulation/traits.h
@@ -0,0 +1,23 @@
+#ifndef TRAITS_H
+#define TRAITS_H
+
+namespace traits {
+	struct Fixed {
+    int reproductive_rate;
+    int decay_rate;
+    int sex;
+    int vision_radius;
+
+    explicit Fixed(int repro_rate, int decay_rate, int sex, int vision);
+  };
+
+  struct Dynamic {
+    int food;
+    int water;
+    int sleep;
+
+    explicit Dynamic();
+  };
+}
+
+#endif  // TRAITS_H
