37a1b50057cbcb4316034d48d7f9155f41dd69bf
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-02 17:48:51 -0600

    added proof of concept test

--- ecosystem-simulation/main.cpp
+++ ecosystem-simulation/main.cpp
@@ -1,10 +1,16 @@
 #include "cell.h"
+#include "creature.h"
 #include "grid.h"
+#include "traits.h"
 #include <ios>
 #include <iostream>
+#include <memory>
 
 int main() {
   Grid grid{25, 25};
-  Cell& cell{grid.get_cell(15, 20)};
-  std::cout << std::boolalpha << cell.is_occupied();
+  Creature creature{traits::Fixed{5, 5, traits::Sex::Male, 10}};
+
+  grid.set_cell(24, 6, std::make_unique<Creature>(creature));
+  std::cout << std::boolalpha << grid.get_cell(24, 6).is_occupied() << '\n';
+  std::cout << grid.get_cell(24, 6).entity;
 }
