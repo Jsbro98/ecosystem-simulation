922e4b3045e8ed5d800b25f964b894e1af015b78
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-05 23:05:00 -0600

    added Position to the Creature class

--- ecosystem-simulation/creature.h
+++ ecosystem-simulation/creature.h
@@ -1,8 +1,10 @@
 #ifndef CREATURE_H
 #define CREATURE_H
 
+#include "attributes.h"
 #include "traits.h"
 
+using attributes::Position;
 typedef traits::Fixed FixedProperties;
 typedef traits::Dynamic DynamicState;
 
@@ -10,12 +12,14 @@ class Creature {
   protected:
     DynamicState dynamic;
     FixedProperties fixed;
+    Position position;
 
   public:
     virtual ~Creature() = default;
     Creature() = delete;
 
-    inline explicit Creature(const FixedProperties& properties) : fixed(properties), dynamic() {};
+    inline explicit Creature(const FixedProperties& properties)
+      : fixed(properties), dynamic(), position() {};
 };
 
 #endif  // CREATURE_H
