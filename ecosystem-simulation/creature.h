7c92bf59dbeb5c99fd4c9e5376386042243f9b39
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-10 19:57:38 -0600

    added virtual reproduce method

--- ecosystem-simulation/creature.h
+++ ecosystem-simulation/creature.h
@@ -3,27 +3,29 @@
 
 #include "attributes.h"
 #include "traits.h"
+#include <memory>
 
 using attributes::Position;
 typedef traits::Fixed FixedProperties;
 typedef traits::Dynamic DynamicState;
 
 class Creature {
-  protected:
-    DynamicState dynamic;
-    FixedProperties fixed;
-    Position position;
-    bool active;
+ protected:
+  DynamicState dynamic;
+  FixedProperties fixed;
+  Position position;
+  bool active;
 
-  public:
-    virtual ~Creature() = default;
-    Creature() = delete;
+ public:
+  virtual ~Creature() = default;
+  Creature() = delete;
 
-    inline explicit Creature(const FixedProperties& properties)
+  inline explicit Creature(const FixedProperties& properties)
       : fixed(properties), dynamic(), position(), active(false) {};
 
-    inline Position& get_position() { return position; };
-    inline bool is_active() const { return active; };
+  inline Position& get_position() { return position; };
+  inline bool is_active() const { return active; };
+  virtual std::unique_ptr<Creature> reproduce();
 };
 
 #endif  // CREATURE_H
